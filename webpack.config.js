99e58ad9fc32adadcb1bed15d101ad91961115fb
Phil Mass <philrmass@gmail.com>
2020-04-06 18:33:57 -0700

    update texture path in dist

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-          'file-loader',
+          'file-loader?outputPath=./data/textures/',
         ],
       },
     ],
