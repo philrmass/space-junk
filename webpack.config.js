a01553df7797e1e2431994f4a03adb360f02ef94
Phil Mass <philrmass@gmail.com>
2020-04-07 23:08:13 -0700

    add webpack public path

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         loader: 'file-loader',
+        options: {
+          publicPath: 'space-junk'
+        },
       },
     ],
   },
